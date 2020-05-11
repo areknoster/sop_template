bcf0a88f44afe32e9133da7290912d0256b1481e
Arkadiusz Noster <arkadiusz.noster@gmail.com>
2020-05-11 14:18:01 +0200

    generalize script

--- tar_and_upload.sh
+++ tar_and_upload.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 LOGIN=your_username
-MAINFILE=main
+FILES=main.c
 
 if test -z "$1"
 then
     echo STAGE argument not given
 else 
 	echo MAKING STAGE${1} TARBALL
-	tar -cjf ${LOGIN}.etap${1}.tar.bz2 ${MAINFILE}.c
+	tar -cjf ${LOGIN}.etap${1}.tar.bz2 ${FILES}
 	echo CONTENT OF ${LOGIN}.etap${1}.tar.bz2 :
-	tar -tjf ${LOGIN}.etap${1}.tar.bz2 ${MAINFILE}.c
+	tar -tjf ${LOGIN}.etap${1}.tar.bz2 ${FILES}
 	scp ${LOGIN}.etap${1}.tar.bz2 ${LOGIN}@ssh.mini.pw.edu.pl:/home2/samba/borkowskim/unix/
 fi
